5eaf0522c5144bce5e715d4594a6eb24174c7457
PRACHI-RAY-5947 <149695789+PRACHI-RAY-5947@users.noreply.github.com>
2024-03-19 11:23:40 +0530

    4-1

---
+++ 4-1/4_1_4.dart
@@ -0,0 +1,11 @@
+double calculateSum(double n1, double n2 , double n3)
+{
+  return n1 + n2 + n3 ;
+}
+void main(){
+
+  double result = calculateSum(10, 40, 30);
+
+  print("Sum of number:$result");
+
+}
