b9c4c1f67d5f1c15d4874c8e6a0d251d1dd5cde3
PRACHI-RAY-5947 <raymuski.002@gmail.com>
2024-03-21 12:25:41 +0530

    e_commerce

--- e_comm_pr1/lib/views/screens/home_Page.dart
+++ e_comm_pr1/lib/views/screens/home_Page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import '../../utils/list_file.dart';
@@ -20,64 +21,277 @@ class _homePageState extends State<homePage> {
     double w = size.width;
 
     return Scaffold(
+      drawer: Drawer(
+        child: Column(children: [
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue.shade200,
+            ),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: Colors.blue.shade800,
+            ),
+            accountName: const Text(
+              "Your Name",
+              style: TextStyle(color: Colors.black),
+            ),
+            accountEmail: const Text(
+              "email@demo.io",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          //home
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: h * 0.04,
+            width: w * 0.8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.blue.shade200,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Row(
+                    children: [
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      const Icon(
+                        Icons.home,
+                        size: 20,
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      const Text(
+                        "Home",
+                      ),
+                      const SizedBox(
+                        width: 175,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.arrow_forward_ios,
+                          size: 15,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //Category
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Icon(
+                      Icons.category_outlined,
+                      size: 20,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Text(
+                      "Category",
+                    ),
+                    const SizedBox(
+                      width: 175,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          //notification
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Icon(
+                      Icons.category_outlined,
+                      size: 20,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Text(
+                      "Category",
+                    ),
+                    const SizedBox(
+                      width: 175,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          // Cart
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Icon(
+                      Icons.category_outlined,
+                      size: 20,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Text(
+                      "Category",
+                    ),
+                    const SizedBox(
+                      width: 175,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          //Orders
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Icon(
+                      Icons.category_outlined,
+                      size: 20,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    const Text(
+                      "Category",
+                    ),
+                    const SizedBox(
+                      width: 175,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.arrow_forward_ios,
+                        size: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ]),
+      ),
       appBar: AppBar(
-        leading: const Icon(Icons.menu),
         centerTitle: true,
-        actions: [
-              Icon(Icons.home)
-         ],
+        actions: [const Icon(Icons.shopping_cart)],
         title: const Text(
           "Shop here",
         ),
         backgroundColor: Colors.blue.shade200,
       ),
-
       body: Container(
         height: h,
         width: w,
         color: Colors.blue.shade900,
         child: SingleChildScrollView(
-          child: Column(children: [
-            ...List.generate(
-              Product.length,
-              (index) => Row(children: [
-                GestureDetector(
-                  onTap: {
-                    Navigator.of(context).pushNamed(MyRoutes.detailPage);
-              },
-                  child: Container(
-                    height: h * 0.3,
-                    width: w * 0.9,
-                    alignment: Alignment.topLeft,
-                    margin: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15.0),
-                      color: Colors.blue.shade200,
-                      shape: BoxShape.rectangle,
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Text(
-                            Product[index]['title'],
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(children: [
+              ...Product.map(
+                (e) => Row(children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pushNamed(MyRoutes.detailPage);
+                    },
+                    child: Container(
+                      height: h * 0.3,
+                      width: w * 0.9,
+                      alignment: Alignment.topLeft,
+                      margin: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15.0),
+                        color: Colors.blue.shade200,
+                        shape: BoxShape.rectangle,
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Text(
+                            e['title'],
                             style: TextStyle(
                               color: Colors.blue.shade900,
                               fontSize: 15,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
-                        ),
-                        Image.network(
-                            "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"),
-                      ],
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Container(
+                            height: h * 0.22,
+                            width: w * 0.4,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  e['thumbnail'],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              ]),
-            ),
-          ]),
+                  )
+                ]),
+              ).toList(),
+            ]),
+          ),
         ),
       ),
     );
